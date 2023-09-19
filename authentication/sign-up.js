b0adbdcc15311ebdb6787fc69bc25392d90c9beb
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-09-19 17:37:07 -0300

    adding gitignore

--- authentication/sign-up.js
+++ authentication/sign-up.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/sign-up', (req, res)=> {
+    query = 'INSERT INTO users_school(name, email, password) VALUES (?,?,?)';
     const {username, email, password} = req.body;
     console.log('teste');
     db.query(
-        'INSERT INTO users_school(name, email, password) VALUES (?,?,?)',
+        query,
         [username, email, password],
         (err, results) => {
             if(err){
@@ -15,9 +16,14 @@ router.post('/sign-up', (req, res)=> {
             }else if(results.length ===1){
                 res.json({message: 'Usuário criado com Sucesso!'});
             }else{
+                console.log(query);
+
+                console.log(username, email, password);
+
                 res.status(401).json({message: 'Credenciais inválidas'});
             }
         }
     )
 })
+
 module.exports = router;
