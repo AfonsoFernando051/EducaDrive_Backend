480e651f1e18caea955cb6afaa23082c7d87c779
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-09-21 00:25:09 -0300

    criei APIs para inserir e ler professores

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const app = express();
 const port = 3000;
 const authRouterLogin = require('./authentication/auth');
 const authRouterSignUp = require('./authentication/sign-up');
+const readProfessores = require('./GetApi/read-professores');
+const insertProfessores = require('./PostApi/create-professor');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRouterLogin);
 app.use('/auth', authRouterSignUp);
+app.use('/read', readProfessores);
+app.use('/insert', insertProfessores);
+
 
 app.listen(port, () => {
     console.log('Server listening on port ${port}');
