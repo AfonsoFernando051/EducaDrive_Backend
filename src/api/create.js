34d34dd14ddea3aa76deeafe8e3bcb11edbb6cbc
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-11-02 18:06:40 -0300

    api de insercao de aulas feita

--- src/api/create.js
+++ src/api/create.js
@@ -4,26 +4,26 @@ module.exports = app => {
         const dados = {...req.body};
 
         app.db('professores')
-        .insert(dados)
-        .then(rows => {
-            res.json(rows)
-        })
-        .catch(error => {
-            res.status(500).json({error: 'Ocorreu um erro ao inserir os dados.'})
-        })
+            .insert(dados)
+            .then(rows => {
+                res.json(rows)
+            })
+            .catch(error => {
+                res.status(500).json({error: 'Ocorreu um erro ao inserir os dados.'})
+            })
     }
 
     const insertAluno = (req, res) => {
         const dados = {...req.body};
 
         app.db('alunos')
-        .insert(dados)
-        .then(rows => {
-            res.json(rows)
-        })
-        .catch(error => {
-            res.status(500).json({error: 'Ocorreu um erro ao inserir os dados.'})
-        })
+            .insert(dados)
+            .then(rows => {
+                res.json(rows)
+            })
+            .catch(error => {
+                res.status(500).json({error: 'Ocorreu um erro ao inserir os dados.'})
+            })
     }
     return {insertProfessor, insertAluno}
 }
